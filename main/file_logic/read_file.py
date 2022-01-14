6685777b02f026fe4f2213ff3e4f196d2a2e94f1
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2022-01-14 12:21:28 -0800

    Updated read_file with a docstring. removed previously tracked pychache file

--- main/file_logic/read_file.py
+++ main/file_logic/read_file.py
@@ -1,3 +1,14 @@
 def read_file(file_name):
+    """Reads the data in a text file
+
+        Parameters
+        ----------
+        file_name : str
+            The name and path of the file to be read
+
+        Returns
+        ------
+            list: A list with each item being a line from the file
+        """
     with open(file_name) as f:
         return f.readlines()
